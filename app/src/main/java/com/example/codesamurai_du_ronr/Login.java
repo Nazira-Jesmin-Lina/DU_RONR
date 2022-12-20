68414dbb81c22fd47e87a5eb816fa41b9d5535f2
syedmumtahinmahmud <2019-617-833@student.cs.du.ac.bd>
2022-12-20 18:10:03 +0600

    Object Class

--- app/src/main/java/com/example/codesamurai_du_ronr/Login.java
+++ app/src/main/java/com/example/codesamurai_du_ronr/Login.java
@@ -6,18 +6,28 @@ import androidx.appcompat.widget.AppCompatButton;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
     AppCompatButton login;
+    TextView signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         login=findViewById(R.id.login_bttn);
+        signup=findViewById(R.id.sign_up_click);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Login.this,HomePage.class);
+                startActivity(intent);
+            }
+        });
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Login.this,SignUp.class);
                 startActivity(intent);
             }
         });
