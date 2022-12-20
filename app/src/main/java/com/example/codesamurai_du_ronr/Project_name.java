cb2e73a0db9838302c8b594bf643ec6550229fb9
Nazira-Jesmin-Lina <83658996+Nazira-Jesmin-Lina@users.noreply.github.com>
2022-12-20 23:12:23 +0600

    other page

---
+++ app/src/main/java/com/example/codesamurai_du_ronr/Project_name.java
@@ -0,0 +1,20 @@
+package com.example.codesamurai_du_ronr;
+
+public class Project_name {
+    private String name;
+
+
+    public Project_name(String name) {
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
